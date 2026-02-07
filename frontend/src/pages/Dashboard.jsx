283fb878c0527f449c345d12bc09f300288f7435
Vikas Sharma <vikas@Vikass-MacBook-Air.local>
2026-02-07 11:25:36 +0530

    charts added

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,17 +1,18 @@
 import { useEffect, useState } from "react";
-
+import GraphModal from "../components/GraphModal";
 import {
   getTimestamps,
   createTimestamp
 } from "../services/api";
 
 import TimestampCard from "../components/TimestampCard";
+import OverallStatsModal from "../components/OverallStatsModal";
 
 export default function Dashboard() {
-
+  const [showOverall, setShowOverall] = useState(false);
   const [timestamps, setTimestamps] = useState([]);
   const [title, setTitle] = useState("");
-
+  const [selectedTimestamp, setSelectedTimestamp] = useState(null);
   const loadData = async () => {
     const data = await getTimestamps();
 
@@ -37,7 +38,7 @@ export default function Dashboard() {
   return (
     <div className="min-h-screen p-4 w-full max-w-md md:max-w-2xl lg:max-w-5xl xl:max-w-7xl mx-auto bg-gradient-to-br from-gray-950 via-gray-900 to-black">
       {/* Header */}
-      <h1 className="text-2xl font-bold mb-4 text-center">
+      <h1 className="text-2xl font-bold mb-4 text-center text-green-400">
         FocusLog
       </h1>
 
@@ -48,7 +49,7 @@ export default function Dashboard() {
           value={title}
           onChange={(e) => setTitle(e.target.value)}
           placeholder="New task..."
-          className="flex-1 p-2 rounded border border-green-600 bg-to-r bg-black"
+          className="flex-1 p-2 rounded bg-gray-900 border border-green-600 text-white placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-green-500"
         />
 
         <button
@@ -56,6 +57,13 @@ export default function Dashboard() {
           className="bg-green-600 text-white px-4 rounded font-semibold"
         >
           Add
+        </button>
+
+        <button
+          onClick={() => setShowOverall(true)}
+          className="bg-green-600 text-white px-4 rounded font-semibold"
+        >
+          📊 Overall
         </button>
 
       </div>
@@ -67,17 +75,32 @@ export default function Dashboard() {
             key={t._id}
             data={t}
             refresh={loadData}
+            onOpenGraph={setSelectedTimestamp}
           />
         ))}
 
 
         {timestamps.length === 0 && (
-          <p className="text-center text-white mt-6">
+          <p className="col-span-full text-center text-gray-400 mt-6">
             No tasks yet. Create one 👆
           </p>
         )}
-      </div>
 
+
+      </div>
+      {showOverall && (
+        <OverallStatsModal
+          timestamps={timestamps}
+          onClose={() => setShowOverall(false)}
+        />
+      )}
+
+      {selectedTimestamp && (
+        <GraphModal
+          data={selectedTimestamp}
+          onClose={() => setSelectedTimestamp(null)}
+        />
+      )}
     </div>
   );
 }
